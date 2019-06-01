a40b65a30c9ef744890508123450fd1b9c1482f6
AlexisHolyoak <peraltaholyoak.aj@gmail.com>
2019-06-01 10:33:00 -0500

    expand nav

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,7 +11,7 @@
         <script type="text/javascript" src="{{ asset('js/app.js') }}" ></script>
 </head>
 <body>
-    <nav class="navbar navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a href="#" class="navbar-brand">Texter</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
