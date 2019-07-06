fcc2030b1a8da8c92199213dfb3559fbbd776d47
AlexisHolyoak <peraltaholyoak.aj@gmail.com>
2019-07-05 21:13:45 -0500

    cambio de interfaz

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Text editor</title>
+    <title>Notes fonts</title>
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     <script src="{{ asset('/vendors/ckeditor/ckeditor.js') }}"></script>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
